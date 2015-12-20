ad1f85f8c2a780a296452f7c3414f831ae0d0f78
Sav10 <sav.derivet@gmail.com>
2015-12-20 07:31:49 +0100

    str lower

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ class Resource(object):
 
     def on_get(self, req, resp):
         ip = req.env['REMOTE_ADDR']
-        id_ = req.get_param("id")
+        id_ = req.get_param("id").lower()
         ip2 = str(ip)
         len_var = len(id_)
 
